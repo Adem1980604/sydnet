a615b260b113e0cce762e8e72b3861ee9134c8ab
medan <meduoliukas12@gmail.com>
2025-03-14 21:06:08 +0100

    ændring :))

--- aktie-app/routes/bruger.js
+++ aktie-app/routes/bruger.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 // vi sætter vores ROUTES op for BRUGER SIDER.
+// GET bruges til at vise log-ind siden 
+
 router.get('/bruger-oprettelse', function(req, res) { 
     res.render('bruger-sider/bruger-oprettelse'); 
 });
@@ -17,6 +19,21 @@ router.get('/indsænder', function(req, res) {
 router.get('/log-ind', function(req, res) { 
     res.render('bruger-sider/log-ind'); 
 });
+
+//POST-rute vil håndeter vores Login-data 
+router.post('/log-ind',(req,res) => {
+    console.log(req.body);
+  const  {brugernavn, adgangskode} = req.body;
+
+  if (brugernavn === "m" && adgangskode === "f") { 
+    res.json({success: true, message: "Login Succesfuld"});
+} else { 
+
+// dette sørger for at API ikke returnerer 200 som vil indikere at logind er OK
+    res.status(401).json({success: false, message: "Forkert brugernavn eller adgangskode"});
+}
+});
+
 
 router.get('/nulstill', function(req, res) { 
     res.render('bruger-sider/nulstill'); 
