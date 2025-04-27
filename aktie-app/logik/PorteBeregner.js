ede87b34bdbd7a07ed7184e610fddd93e5d6a012
Emil-indiantechsupport <empa24af@student.cbs.dk>
2025-04-27 17:28:41 +0200

    Fejl fikset

--- aktie-app/logik/PorteBeregner.js
+++ aktie-app/logik/PorteBeregner.js
@@ -18,7 +18,7 @@ class PortefoljeBeregner {
 
     // Metode: Beregner ejerliste og GAK for alle aktier
     beregnEjerOgGAK() {
- // Gennemgår alle handler (køb/salg)
+        // Gennemgår alle handler (køb/salg)
         for (let i = 0; i < this.handler.length; i++) {
             const h = this.handler[i];
             const symbol = h.symbol;
@@ -28,7 +28,7 @@ class PortefoljeBeregner {
 
 
 
-     // 1: Find aktien i ejerlisten (hvis den findes)
+            // 1: Find aktien i ejerlisten (hvis den findes)
             let ejerAktie = null;
             for (let j = 0; j < this.ejerListe.length; j++) {
                 if (this.ejerListe[j].symbol === symbol) {
@@ -36,21 +36,21 @@ class PortefoljeBeregner {
                     break;
                 }
             }
-  // Hvis ikke fundet, tilføj ny aktie til ejerliste
+            // Hvis ikke fundet, tilføj ny aktie til ejerliste
             if (!ejerAktie) {
                 ejerAktie = { symbol: symbol, antal: 0 };
                 this.ejerListe.push(ejerAktie);
             }
 
-  
- // 2: Find eller opret GAK beregning for aktien
+
+            // 2: Find eller opret GAK beregning for aktien
             let gakAktie = this.gakBeregning.find(g => g.symbol === symbol);
             if (!gakAktie) {
                 gakAktie = { symbol, samletAntal: 0, samletPris: 0 };
                 this.gakBeregning.push(gakAktie);
             }
 
-  // 3: Opdater ejerantal og GAK baseret på køb/salg
+            // 3: Opdater ejerantal og GAK baseret på køb/salg
             if (salg_koeb === false) { // køb
                 ejerAktie.antal += antal;
                 gakAktie.samletPris += antal * pris;
@@ -63,30 +63,30 @@ class PortefoljeBeregner {
             }
         }
 
-// Fjern aktier hvor antal = 0 (altså brugeren ejer dem ikke mere)
+        // Fjern aktier hvor antal = 0 (altså brugeren ejer dem ikke mere)
         this.ejerListeFiltreret = this.ejerListe.filter(e => e.antal > 0);
 
- // For hver aktie vi ejer:
-// Find den nyeste pris, navn og udregn GAK
+        // For hver aktie vi ejer:
+        // Find den nyeste pris, navn og udregn GAK
         for (let i = 0; i < this.ejerListeFiltreret.length; i++) {
             const e = this.ejerListeFiltreret[i];
 
 
-      // Find nyeste handel for aktien       
-         let senesteHandler = null;
-for (let j = 0; j < this.handler.length; j++) {
-    if (this.handler[j].symbol === e.symbol) {
-        if (!senesteHandler || new Date(this.handler[j].dato) > new Date(senesteHandler.dato)) {
-            senesteHandler = this.handler[j];
-        }
-    }
-}
+            // Find nyeste handel for aktien       
+            let senesteHandler = null;
+            for (let j = 0; j < this.handler.length; j++) {
+                if (this.handler[j].symbol === e.symbol) {
+                    if (!senesteHandler || new Date(this.handler[j].dato) > new Date(senesteHandler.dato)) {
+                        senesteHandler = this.handler[j];
+                    }
+                }
+            }
 
-       // Hvis vi fandt en nyeste handel -> brug pris og navn
-if (senesteHandler) {
-    e.pris = senesteHandler.pris;
-    e.navn = senesteHandler.navn;
-}
+            // Hvis vi fandt en nyeste handel -> brug pris og navn
+            if (senesteHandler) {
+                e.pris = senesteHandler.pris;
+                e.navn = senesteHandler.navn;
+            }
 
             // Find GAK
             for (let j = 0; j < this.gakBeregning.length; j++) {
@@ -107,11 +107,11 @@ if (senesteHandler) {
         // går igennem alle værdipapir ( altså dem vi stadig ejer)
         for (let i = 0; i < this.ejerListeFiltreret.length; i++) {
             const e = this.ejerListeFiltreret[i];
-            totalErhvervelsespris += e.antal * e.gak; 
-            totalForventetVaerdi += e.antal * e.pris; 
+            totalErhvervelsespris += e.antal * e.gak;
+            totalForventetVaerdi += e.antal * e.pris;
         }
 
-          // Beregn urealiseret gevinst/tab (profit minus hvad vi har betalt)
+        // Beregn urealiseret gevinst/tab (profit minus hvad vi har betalt)
         const totalUrealiseretGevinstTab = totalForventetVaerdi - totalErhvervelsespris;
 
         return {
